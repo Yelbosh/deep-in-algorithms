27b1191e401548ee28c27d52b431308eea1c1c71
Yelbosh <731987620@qq.com>
2016-06-20 18:11:57 +0800

    Bubble排序实现1

--- src/main/java/com/yelbosh/algorithm/App.java
+++ src/main/java/com/yelbosh/algorithm/App.java
@@ -3,7 +3,9 @@ package com.yelbosh.algorithm;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
+import com.yelbosh.algorithm.sort.bubblesort.BubbleSort;
 import com.yelbosh.algorithm.sort.quicksort.QuickSort;
+import com.yelbosh.algorithm.util.ArrayUtil;
 import com.yelbosh.algorithm.util.LoggerUtil;
 
 /**
@@ -15,6 +17,10 @@ public class App
 	//测试入口，请在该处进行代码测试，只在该处调用算法类的其中一个接口
     public static void main( String[] args )
     {
-    	QuickSort.test();
+    	long starTime = System.currentTimeMillis();
+    	BubbleSort.test();
+    	long endTime = System.currentTimeMillis();
+		long time = endTime - starTime;
+    	System.out.println(time);
     }
 }
--- src/main/java/com/yelbosh/algorithm/sort/bubblesort/BubbleSort.java
+++ src/main/java/com/yelbosh/algorithm/sort/bubblesort/BubbleSort.java
@@ -1,5 +1,33 @@
 package com.yelbosh.algorithm.sort.bubblesort;
 
-public class BubbleSort {
+import java.util.Arrays;
 
+import org.apache.log4j.Logger;
+
+import com.yelbosh.algorithm.sort.quicksort.QuickSort;
+import com.yelbosh.algorithm.util.ArrayUtil;
+import com.yelbosh.algorithm.util.LoggerUtil;
+
+public class BubbleSort {
+	
+	//测试入口，在main中调用该方法
+	public static void test(){
+		Logger logger = LoggerUtil.getLogger(QuickSort.class);
+		int[] s = {8,9,5,1,8,6,3,4,2,0};
+		logger.info("要排序的数组："+Arrays.toString(s));
+		bubblesort_1(s, 10);
+		logger.info("排序后的数组："+Arrays.toString(s));
+	}
+	
+	//冒泡排序实现算法1
+	public static void bubblesort_1(int[] s, int n){
+		for(int i=n-1;i>0;i--){
+			for(int j=0;j<i;j++){
+				if(s[j] > s[j+1])
+					ArrayUtil.swap(s, j, j+1);
+			}
+		}
+	}
+	
+	//
 }
