ef06dd69e9d640ebe730c351254cdaee5a476c3b
YaboHome <yelbosh@foxmail.com>
2016-06-21 17:46:42 +0800

    leetcode初shi化

---
+++ src/main/java/com/yelbosh/algorithm/leetcode/easy/StringOptr.java
@@ -0,0 +1,11 @@
+package com.yelbosh.algorithm.leetcode.easy;
+
+public class StringOptr {
+	
+	//µ¹×ª×Ö·û´®
+	public static String reverseString(String s) {
+        return new StringBuffer(s).reverse().toString();
+    }
+	
+	
+}
